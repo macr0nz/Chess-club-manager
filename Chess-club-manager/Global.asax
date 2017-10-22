7b9ba9e741e2d2280b3d83797cd57f05a8d17638
Victor Osipov <macr0nz@yandex.ru>
2017-10-22 18:33:07 +0300

    -initial Project commit -added main project -added dataModel project -updated nuget packages

---
+++ Chess-club-manager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chess_club_manager.MvcApplication" Language="C#" %>
---
+++ Chess-club-manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chess_club_manager.Startup))]
+namespace Chess_club_manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
