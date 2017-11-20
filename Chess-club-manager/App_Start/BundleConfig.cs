10f4b5f256436c74a59156bb9e29ffde1a3f2b6b
Victor Osipov <macr0nz@yandex.ru>
2017-11-20 23:38:12 +0200

    -changed bootstrap to custombootstrap (fixed host making default bootstrap) -fixed home page tournaments table responsiveness

--- Chess-club-manager/App_Start/BundleConfig.cs
+++ Chess-club-manager/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace Chess_club_manager
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
+                "~/Scripts/custombootstrap.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
+                "~/Content/custombootstrap.css",
                 "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome.css"));
