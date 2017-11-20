8fe61eae2cd6029d53ebfe36bd53775ff1b7839a
Victor Osipov <macr0nz@yandex.ru>
2017-11-21 00:13:41 +0200

    -added DateTimeHelper -added custom date format -custom date format applied to UI dates -disabled UI button to remove TrnCreator from this Trn Arbitrators

---
+++ Chess-club-manager/Helpers/DateTimeHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess_club_manager.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static string ToUIformat(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+            {
+                return string.Empty;
+            }
+
+            var dateTimeNotNull = dateTime.Value;
+
+            return dateTimeNotNull.ToString("dd-MMM-yyyy hh:mm");
+        }
+
+        public static string ToUIformat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd-MMM-yyyy hh:mm");
+        }
+
+        public static string ToShortDateUI(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+            {
+                return string.Empty;
+            }
+
+            var dateTimeNotNull = dateTime.Value;
+
+            return dateTimeNotNull.ToString("dd-MMM-yyyy");
+        }
+    }
+}
